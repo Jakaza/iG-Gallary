e5afa88ad8f2eb54b8bcd13344e3bce92d81ba2e
Themba G <goodnessthembac@gmail.com>
2023-02-24 01:26:40 +0200

    Add files
    Install some packagies and initiate a simple server using express
    Add .ignore file to prevent exposing some data.
    add evn file to save sensitive data

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log()[D""[DHello World this is server");
+const express = require("express")
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT_NO | 3000;
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "This is home page"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server runing at port ${PORT}`);
+})
+
+
+
+
+
+
