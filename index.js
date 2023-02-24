4b1d57978f8556abcf349247b9c8650722241434
Themba G <goodnessthembac@gmail.com>
2023-02-24 16:40:32 +0200

    init of upload image to cloudinary and to db

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/", router)
 
+
 app.listen(PORT, () => {
     console.log(`Server runing at port ${PORT}`);
 })
